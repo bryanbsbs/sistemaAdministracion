3c5a4c49c95b61a5689fac94a1b59370796492e1
Bryan Garcia <brusbsbsjlll@gmail.com>
2022-06-07 11:57:03 -0500

    Implementacion de auditing laravel

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use OwenIt\Auditing\Auditable as AuditingAuditable;
+use OwenIt\Auditing\Contracts\Auditable;
 
 
-class Transaction extends Model
+class Transaction extends Model implements Auditable
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, AuditingAuditable;
 
     protected $table = 'transactions';
 
